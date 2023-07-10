d2602241942e1a3a89e5b3aa31c46c27627126b5
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-10 18:50:57 +0530

    Added CORS policy for hello.ts api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,14 @@
 import clientPromise from "./_connector";
 
-export default async (req, res) => {
+import { NextApiRequest, NextApiResponse } from "next";
+import microCors from "micro-cors";
+
+const cors = microCors();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await clientPromise;
 
   res.status(200).json({ name: "John Doe" });
 };
+
+export default cors(handler);
