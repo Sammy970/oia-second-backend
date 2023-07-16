bc67e67e7de1c8e456bc5e1982feb3c6648be5f5
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-17 03:40:31 +0530

    Added CLick analytics saving in mongodb in fetchLinks

--- pages/api/fetchLinks.ts
+++ pages/api/fetchLinks.ts
@@ -10,15 +10,60 @@ export default async (req, res) => {
       [req.body.data]: { $exists: true },
     });
     if (linkData !== null) {
-      // console.log(linkData);
       res.statusCode = 201;
-      return res.json(linkData);
+      res.json(linkData);
+
+      linkData[req.body.data].clicks = linkData[req.body.data].clicks + 1;
+
+      // console.log(linkData);
+
+      const result = await db
+        .db("Data")
+        .collection("getCodes")
+        .replaceOne({ _id: linkData["_id"] }, linkData);
+
+      if (result.modifiedCount === 1) {
+        const usersDB = await db.db("Data").collection("users");
+        const documents = await usersDB.find({}).toArray();
+
+        let index;
+        let email;
+        documents.map((data) => {
+          index = data[Object.keys(data)[1]].findIndex((d) =>
+            d.hasOwnProperty(req.body.data)
+          );
+          if (index !== -1) {
+            email = Object.keys(data)[1];
+            return;
+          }
+        });
+
+        // console.log(index);
+        // console.log(email);
+
+        const indexOfDoc = documents.findIndex((d) => d.hasOwnProperty(email));
+
+        const objToBeUpdated =
+          documents[indexOfDoc][email][index][req.body.data];
+        objToBeUpdated.clicks = objToBeUpdated.clicks + 1;
+
+        // console.log(documents[indexOfDoc]._id);
+
+        const result = await usersDB.replaceOne(
+          {
+            _id: documents[indexOfDoc]._id,
+          },
+          documents[indexOfDoc]
+        );
+      } else {
+        console.log("Error in updating clicks");
+      }
     } else {
       res.statusCode = 404;
       return res.json({ error: "Code not found" });
     }
+  } else {
+    res.statusCode = 409;
+    return res.json({ error: "Send some Data when using Post" });
   }
-
-  res.statusCode = 409;
-  res.json({ error: "Send some Data when using Post" });
 };
