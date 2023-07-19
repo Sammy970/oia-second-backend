189dc83ead2c033fbabf77c3ae2de4492682d1a1
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-07-19 10:48:59 +0530

    When sharing links on platforms like whatsapp - fixed showing undefined as osName

--- pages/api/fetchLinks.ts
+++ pages/api/fetchLinks.ts
@@ -9,131 +9,138 @@ export default async (req, res) => {
     const linkData = await fetchLinkData.findOne({
       [req.body.data]: { $exists: true },
     });
+
     if (linkData !== null) {
-      linkData[req.body.data].clicks = linkData[req.body.data].clicks + 1;
-      if (
-        linkData[req.body.data].fromWhere === undefined ||
-        linkData[req.body.data].fromWhere === null
-      ) {
-        console.log("I am in here");
-        // console.log(req.body.city);
-        linkData[req.body.data].fromWhere = { city: [{ [req.body.city]: 1 }] };
-        console.log(linkData[req.body.data]);
-      } else if (
-        linkData[req.body.data].fromWhere.city === undefined &&
-        linkData[req.body.data].fromWhere.state === undefined &&
-        linkData[req.body.data].fromWhere.country === undefined &&
-        linkData[req.body.data].fromWhere.osName === undefined
-      ) {
-        linkData[req.body.data].fromWhere.city = [];
-        linkData[req.body.data].fromWhere.state = [];
-        linkData[req.body.data].fromWhere.country = [];
-        linkData[req.body.data].fromWhere.osName = [];
-      }
-
-      console.log("I am in else");
-      const findIndCity = linkData[req.body.data].fromWhere.city.findIndex(
-        (cityData) => {
-          return Object.keys(cityData).toString() === `${req.body.city}`;
+      if (req.body.osName !== undefined || req.body.osName !== null) {
+        linkData[req.body.data].clicks = linkData[req.body.data].clicks + 1;
+        if (
+          linkData[req.body.data].fromWhere === undefined ||
+          linkData[req.body.data].fromWhere === null
+        ) {
+          console.log("I am in here");
+          // console.log(req.body.city);
+          linkData[req.body.data].fromWhere = {
+            city: [{ [req.body.city]: 1 }],
+          };
+          console.log(linkData[req.body.data]);
+        } else if (
+          linkData[req.body.data].fromWhere.city === undefined &&
+          linkData[req.body.data].fromWhere.state === undefined &&
+          linkData[req.body.data].fromWhere.country === undefined &&
+          linkData[req.body.data].fromWhere.osName === undefined
+        ) {
+          linkData[req.body.data].fromWhere.city = [];
+          linkData[req.body.data].fromWhere.state = [];
+          linkData[req.body.data].fromWhere.country = [];
+          linkData[req.body.data].fromWhere.osName = [];
         }
-      );
 
-      const findIndState = linkData[req.body.data].fromWhere.state.findIndex(
-        (stateData) => {
-          return Object.keys(stateData).toString() === `${req.body.state}`;
-        }
-      );
+        console.log("I am in else");
+        const findIndCity = linkData[req.body.data].fromWhere.city.findIndex(
+          (cityData) => {
+            return Object.keys(cityData).toString() === `${req.body.city}`;
+          }
+        );
 
-      const findIndCountry = linkData[
-        req.body.data
-      ].fromWhere.country.findIndex((countryData) => {
-        return Object.keys(countryData).toString() === `${req.body.country}`;
-      });
+        const findIndState = linkData[req.body.data].fromWhere.state.findIndex(
+          (stateData) => {
+            return Object.keys(stateData).toString() === `${req.body.state}`;
+          }
+        );
 
-      const findIndOsName = linkData[req.body.data].fromWhere.osName.findIndex(
-        (osNameData) => {
+        const findIndCountry = linkData[
+          req.body.data
+        ].fromWhere.country.findIndex((countryData) => {
+          return Object.keys(countryData).toString() === `${req.body.country}`;
+        });
+
+        const findIndOsName = linkData[
+          req.body.data
+        ].fromWhere.osName.findIndex((osNameData) => {
           return Object.keys(osNameData).toString() === `${req.body.osName}`;
+        });
+
+        // console.log(findIndex);
+        // console.log(req.body.city);
+
+        // City --------------------------------------------
+
+        if (findIndCity !== -1) {
+          const getKeyName = Object.keys(
+            linkData[req.body.data].fromWhere.city[findIndCity]
+          ).toString();
+
+          const currentVal =
+            linkData[req.body.data].fromWhere.city[findIndCity][req.body.city];
+          const newVal = currentVal + 1;
+          linkData[req.body.data].fromWhere.city[findIndCity][req.body.city] =
+            newVal;
+        } else {
+          linkData[req.body.data].fromWhere.city.push({ [req.body.city]: 1 });
         }
-      );
 
-      // console.log(findIndex);
-      // console.log(req.body.city);
+        // State --------------------------------------------
 
-      // City --------------------------------------------
+        if (findIndState !== -1) {
+          const getKeyName = Object.keys(
+            linkData[req.body.data].fromWhere.state[findIndState]
+          ).toString();
 
-      if (findIndCity !== -1) {
-        const getKeyName = Object.keys(
-          linkData[req.body.data].fromWhere.city[findIndCity]
-        ).toString();
+          const currentVal =
+            linkData[req.body.data].fromWhere.state[findIndState][
+              req.body.state
+            ];
+          const newVal = currentVal + 1;
+          linkData[req.body.data].fromWhere.state[findIndState][
+            req.body.state
+          ] = newVal;
+        } else {
+          linkData[req.body.data].fromWhere.state.push({ [req.body.state]: 1 });
+        }
 
-        const currentVal =
-          linkData[req.body.data].fromWhere.city[findIndCity][req.body.city];
-        const newVal = currentVal + 1;
-        linkData[req.body.data].fromWhere.city[findIndCity][req.body.city] =
-          newVal;
-      } else {
-        linkData[req.body.data].fromWhere.city.push({ [req.body.city]: 1 });
-      }
+        // Country --------------------------------------------
 
-      // State --------------------------------------------
+        if (findIndCountry !== -1) {
+          const getKeyName = Object.keys(
+            linkData[req.body.data].fromWhere.country[findIndCountry]
+          ).toString();
 
-      if (findIndState !== -1) {
-        const getKeyName = Object.keys(
-          linkData[req.body.data].fromWhere.state[findIndState]
-        ).toString();
-
-        const currentVal =
-          linkData[req.body.data].fromWhere.state[findIndState][req.body.state];
-        const newVal = currentVal + 1;
-        linkData[req.body.data].fromWhere.state[findIndState][req.body.state] =
-          newVal;
-      } else {
-        linkData[req.body.data].fromWhere.state.push({ [req.body.state]: 1 });
-      }
-
-      // Country --------------------------------------------
-
-      if (findIndCountry !== -1) {
-        const getKeyName = Object.keys(
-          linkData[req.body.data].fromWhere.country[findIndCountry]
-        ).toString();
-
-        const currentVal =
+          const currentVal =
+            linkData[req.body.data].fromWhere.country[findIndCountry][
+              req.body.country
+            ];
+          const newVal = currentVal + 1;
           linkData[req.body.data].fromWhere.country[findIndCountry][
             req.body.country
-          ];
-        const newVal = currentVal + 1;
-        linkData[req.body.data].fromWhere.country[findIndCountry][
-          req.body.country
-        ] = newVal;
-      } else {
-        linkData[req.body.data].fromWhere.country.push({
-          [req.body.country]: 1,
-        });
-      }
+          ] = newVal;
+        } else {
+          linkData[req.body.data].fromWhere.country.push({
+            [req.body.country]: 1,
+          });
+        }
 
-      // osName --------------------------------------------
+        // osName --------------------------------------------
 
-      if (findIndOsName !== -1) {
-        const getKeyName = Object.keys(
-          linkData[req.body.data].fromWhere.osName[findIndOsName]
-        ).toString();
+        if (findIndOsName !== -1) {
+          const getKeyName = Object.keys(
+            linkData[req.body.data].fromWhere.osName[findIndOsName]
+          ).toString();
 
-        const currentVal =
+          const currentVal =
+            linkData[req.body.data].fromWhere.osName[findIndOsName][
+              req.body.osName
+            ];
+          const newVal = currentVal + 1;
           linkData[req.body.data].fromWhere.osName[findIndOsName][
             req.body.osName
-          ];
-        const newVal = currentVal + 1;
-        linkData[req.body.data].fromWhere.osName[findIndOsName][
-          req.body.osName
-        ] = newVal;
-      } else {
-        linkData[req.body.data].fromWhere.osName.push({
-          [req.body.osName]: 1,
-        });
+          ] = newVal;
+        } else {
+          linkData[req.body.data].fromWhere.osName.push({
+            [req.body.osName]: 1,
+          });
+        }
+        // console.log(linkData);
       }
-
-      // console.log(linkData);
 
       const result = await db
         .db("Data")
